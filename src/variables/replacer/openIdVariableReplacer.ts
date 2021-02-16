fbeaefd1152b13e95db25745624d7529bba34ff3
Andreas Weber <weber.andreas@gmail.com>
2021-02-16 11:47:53 +0100

    fix: error in isTokenExpired fixed. keep-alive added

--- src/variables/replacer/openIdVariableReplacer.ts
+++ src/variables/replacer/openIdVariableReplacer.ts
@@ -1,4 +1,4 @@
-import { ProcessorContext, HttpResponse, HttpClientOptions } from '../../models';
+import { ProcessorContext, HttpResponse, HttpClientOptions, HttpClient, Progress } from '../../models';
 import { isString, toConsoleOutput, decodeJWT } from '../../utils';
 import { environmentStore } from '../../environments';
 import { log } from '../../logger';
@@ -19,24 +19,29 @@ interface OpenIdConfig{
 }
 
 interface OpenIdInformation{
+  clientId: string;
+  clientSecret: string;
   url: string;
   accessToken: string;
   expiresIn: number;
   time: number;
   refreshToken?: string;
-  refreshExpiresIn?: number
+  refreshExpiresIn?: number;
+  timeSkew: number;
+  dispose?: () => void;
 }
 
 const oauthStore: Record<string, OpenIdInformation> = {};
 environmentStore.additionalResets.push(() => {
   for (const [key] of Object.entries(oauthStore)) {
-    delete oauthStore[key];
+    removeOpenIdInformation(key);
   }
 });
 
 
 export async function openIdVariableReplacer(text: string, type: string, context: ProcessorContext) {
   if (type.toLowerCase() === "authorization" && text) {
+    const cacheKey = `${context.httpFile.activeEnvironment?.join('_')}|${text}`;
     try {
       const flows: Record<string, ((config: OpenIdConfig) => Promise<HttpClientOptions | false>) | undefined> = {
         'client_credentials': clientCredentialsFlow,
@@ -45,7 +50,7 @@ export async function openIdVariableReplacer(text: string, type: string, context
         'code': (config) => authorizationCodeFlow(config, context.httpFile.fileName),
       };
 
-      let openIdInformation: OpenIdInformation | false = await refreshToken(text, context);
+      let openIdInformation: OpenIdInformation | false = await refreshTokenOrReuse(oauthStore[cacheKey], context.httpClient, context.progress);
       if (!openIdInformation) {
         const match = /^\s*(openid)\s+(?<flow>[^\s]*)\s+(?<variablePrefix>[^\s]*)\s*((token_exchange)\s+(?<tokenExchangePrefix>[^\s]*))?\s*$/i.exec(text);
         if (match && match.groups && match.groups.flow && match.groups.variablePrefix) {
@@ -57,76 +62,115 @@ export async function openIdVariableReplacer(text: string, type: string, context
             if (openIdInformation && match.groups.tokenExchangePrefix) {
               openIdInformation = await requestOpenIdInformation(match.groups.tokenExchangePrefix, config => tokenExchangeFlow(config, openIdInformation), context);
             }
+            if (!openIdInformation) {
+              throw new Error(`Anmeldung gescheitert`);
+            }
           }
         }
       }
 
       if (openIdInformation) {
-        oauthStore[text] = openIdInformation;
+        removeOpenIdInformation(cacheKey);
+        oauthStore[cacheKey] = openIdInformation;
+        keepAlive(cacheKey, context.httpClient);
         return `Bearer ${openIdInformation.accessToken}`;
       }
     } catch (err) {
-      delete oauthStore[text];
+      removeOpenIdInformation(cacheKey);
       throw err;
     }
   }
   return text;
 }
 
-
-async function refreshToken(text: string, { httpClient, progress }: ProcessorContext) {
-  if (oauthStore[text]) {
-    const authInformation = oauthStore[text];
-    if (!isTokenExpired(authInformation.time, authInformation.expiresIn)) {
-      return authInformation;
-    }
-
-    if (authInformation.refreshToken
-      && authInformation.refreshExpiresIn
-      && !isTokenExpired(authInformation.time, authInformation.refreshExpiresIn)) {
-
-      const time = new Date().getTime();
-      const options: HttpClientOptions = {
-        url: authInformation.url,
-        method: 'POST',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-        body: `grant_type=refresh_token&refresh_token=${authInformation.refreshToken}`
-      };
-      const response = await httpClient(options, progress, false);
-      if (response) {
-        response.request = options;
+function keepAlive(cacheKey: string, httpClient: HttpClient) {
+  const openIdInformation = oauthStore[cacheKey];
+  if (openIdInformation) {
+    const timeoutId = setTimeout(async () => {
+      const result = await refreshToken(openIdInformation, httpClient, undefined);
+      if (result) {
+        oauthStore[cacheKey] = result;
+        keepAlive(cacheKey, httpClient);
       }
-      return toOpenIdInformation(authInformation.url, time, response);
+    }, (openIdInformation.expiresIn - openIdInformation.timeSkew) * 1000);
+    openIdInformation.dispose = () => clearTimeout(timeoutId);
+  }
+}
+
+function removeOpenIdInformation(cacheKey: string) {
+  const cachedOpenIdInformation = oauthStore[cacheKey];
+  if (cachedOpenIdInformation) {
+    if (cachedOpenIdInformation.dispose) {
+      cachedOpenIdInformation.dispose();
     }
+    delete oauthStore[cacheKey];
+  }
+
+
+}
+
+
+async function refreshTokenOrReuse(cachedOpenIdInformation: OpenIdInformation, httpClient: HttpClient, progress: Progress | undefined) {
+  if (cachedOpenIdInformation) {
+    if (!isTokenExpired(cachedOpenIdInformation.time, cachedOpenIdInformation.expiresIn, cachedOpenIdInformation.timeSkew)) {
+      return cachedOpenIdInformation;
+    }
+
+    return await refreshToken(cachedOpenIdInformation, httpClient, progress);
   }
   return false;
 }
 
 
-function toOpenIdInformation(url: string, time: number, response: false | HttpResponse): OpenIdInformation | false {
+async function refreshToken(cachedOpenIdInformation: OpenIdInformation, httpClient: HttpClient, progress: Progress | undefined) {
+  if (cachedOpenIdInformation.refreshToken
+    && cachedOpenIdInformation.refreshExpiresIn
+    && !isTokenExpired(cachedOpenIdInformation.time, cachedOpenIdInformation.refreshExpiresIn, cachedOpenIdInformation.timeSkew)) {
+
+    const time = new Date().getTime();
+    const options: HttpClientOptions = {
+      url: cachedOpenIdInformation.url,
+      method: 'POST',
+      headers: {
+        'authorization': `Basic ${Buffer.from(`${cachedOpenIdInformation.clientId}:${cachedOpenIdInformation.clientSecret}`).toString('base64')}`,
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=refresh_token&refresh_token=${cachedOpenIdInformation.refreshToken}`
+    };
+    const response = await httpClient(options, progress, false);
+    if (response) {
+      response.request = options;
+    }
+    return toOpenIdInformation(cachedOpenIdInformation.url, cachedOpenIdInformation.clientId, cachedOpenIdInformation.clientSecret, time, response);
+  }
+  return false;
+}
+
+function toOpenIdInformation(url: string, clientId: string, clientSecret: string, time: number, response: false | HttpResponse): OpenIdInformation | false {
   if (response) {
     log.info(toConsoleOutput(response, true));
     if (response.statusCode === 200 && isString(response.body)) {
       const jwtToken = JSON.parse(response.body);
-      log.info(JSON.stringify(jwtToken, null, 2));
+      const parsedToken = decodeJWT(jwtToken.access_token);
+      log.info(JSON.stringify(parsedToken, null, 2));
       return {
         url,
+        clientId,
+        clientSecret,
         time,
         accessToken: jwtToken.access_token,
         expiresIn: jwtToken.expires_in,
         refreshToken: jwtToken.refresh_token,
         refreshExpiresIn: jwtToken.refresh_expires_in,
+        timeSkew: Math.floor(time / 1000) - parsedToken.iat,
       };
     }
-    throw new Error(`${response.body}`);
   }
   return false;
 }
 
-function isTokenExpired(time: number, expiresIn: number) {
-  return (time + 1000 * expiresIn) < (new Date()).getTime();
+function isTokenExpired(time: number, expiresIn: number, timeSkew: number) {
+  return time + 1000 * (expiresIn - timeSkew) < (new Date()).getTime();
 }
 
 async function requestOpenIdInformation(variablePrefix: string, getOptions: (config: OpenIdConfig) => Promise<HttpClientOptions | false>, { httpClient, progress, variables }: ProcessorContext) : Promise<OpenIdInformation | false>{
@@ -150,7 +194,7 @@ async function requestOpenIdInformation(variablePrefix: string, getOptions: (con
     if (response) {
       response.request = options;
     }
-    return toOpenIdInformation(config.tokenEndpoint, time, response);
+    return toOpenIdInformation(config.tokenEndpoint, config.clientId, config.clientSecret, time, response);
   }
   return false;
 }
