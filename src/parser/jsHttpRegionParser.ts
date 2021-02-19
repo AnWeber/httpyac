22d5899fd3acb1e80ba1b5834e955b59563a61cf
Andreas Weber <weber.andreas@gmail.com>
2021-02-19 20:30:59 +0100

    feat: execute script on every request

--- src/parser/jsHttpRegionParser.ts
+++ src/parser/jsHttpRegionParser.ts
@@ -4,32 +4,44 @@ import { toMultiLineString } from '../utils';
 import { jsActionProcessor, ScriptData } from '../actionProcessor';
 
 
+const JS_ON_EVERY_REQUEST_IDENTIFIER = 'jsOnEveryRequest';
 export class JsHttpRegionParser implements HttpRegionParser{
-  async parse(lineReader: HttpRegionParserGenerator, { httpRegion }: ParserContext): Promise<HttpRegionParserResult> {
+  async parse(lineReader: HttpRegionParserGenerator, { httpRegion, data }: ParserContext): Promise<HttpRegionParserResult> {
     let next = lineReader.next();
 
     if (!next.done) {
-      const matches = /^\s*{{\s*$/.exec(next.value.textLine);
+      const matches = /^\s*{{(?<executeOnEveryRequest>+)?\s*$/.exec(next.value.textLine);
       if (!matches) {
         return false;
       }
+      const executeOnEveryRequest = !!matches.groups?.executeOnEveryRequest;
       const lineOffset = next.value.line;
       next = lineReader.next();
       const script: Array<string> = [];
       while (!next.done) {
 
         if (/^\s*}}\s*$/.test(next.value.textLine)) {
-          const data: ScriptData = {
+          const scriptData: ScriptData = {
             script: toMultiLineString(script),
             lineOffset,
           };
-          httpRegion.actions.push(
-            {
-              data,
-              type: ActionProcessorType.js,
-              processor: jsActionProcessor,
+
+          if (!executeOnEveryRequest) {
+            httpRegion.actions.push(
+              {
+                data: scriptData,
+                type: ActionProcessorType.js,
+                processor: jsActionProcessor,
+              }
+            );
+          } else {
+
+            let onEveryRequestArray: ScriptData[] = data[JS_ON_EVERY_REQUEST_IDENTIFIER];
+            if (!onEveryRequestArray) {
+              data[JS_ON_EVERY_REQUEST_IDENTIFIER] = onEveryRequestArray = [];
             }
-          );
+            onEveryRequestArray.push(scriptData);
+          }
 
           return {
             endLine: next.value.line,
@@ -49,5 +61,19 @@ export class JsHttpRegionParser implements HttpRegionParser{
       }
     }
     return false;
+  }
+
+  close({data, httpRegion}: ParserContext): void {
+    let onEveryRequestArray: ScriptData[] = data[JS_ON_EVERY_REQUEST_IDENTIFIER];
+    if (onEveryRequestArray && httpRegion.request) {
+      httpRegion.actions.splice(0, 0, ...onEveryRequestArray.map(scriptData => {
+        return {
+          data: scriptData,
+          type: ActionProcessorType.js,
+          processor: jsActionProcessor,
+        };
+      }));
+
+    }
   }
 }
