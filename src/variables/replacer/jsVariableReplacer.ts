0b11791767eef973d7faaa5f241f068a1e12813d
Andreas Weber <weber.andreas@gmail.com>
2021-03-18 13:23:34 +0100

    Fix: error replacing variable with string empty

--- src/variables/replacer/jsVariableReplacer.ts
+++ src/variables/replacer/jsVariableReplacer.ts
@@ -1,6 +1,6 @@
 import { ProcessorContext } from '../../models';
 import { executeScript } from '../../actionProcessor';
-import { toMultiLineArray } from '../../utils';
+import { isString, toMultiLineArray } from '../../utils';
 
 export async function jsVariableReplacer(text: string, type: string, {httpRegion, httpFile, variables, progress}: ProcessorContext) {
   const variableRegex = /\{{2}([^}{2}]+)\}{2}/g;
@@ -26,8 +26,12 @@ export async function jsVariableReplacer(text: string, type: string, {httpRegion
         progress
       }
     });
-    if (value.$result) {
-      result = result.replace(match[0], `${value.$result}`);
+    if (isString(value.$result) || typeof value.$result === 'number') {
+      result = result.replace(searchValue, `${value.$result}`);
+    }else if (value.$result instanceof Date) {
+      result = result.replace(searchValue, `${value.$result.toISOString()}`);
+    }else if (value.$result) {
+      result = result.replace(searchValue, value.$result);
     }
   }
   return result;
