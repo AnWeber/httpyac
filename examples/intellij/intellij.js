0680fdf220b2dd9c3bc4bc2d8683f771bc11788f
Andreas Weber <weber.andreas@gmail.com>
2021-09-21 21:38:22 +0200

    refactor: examples in httpyac.github.io

--- examples/intellij/intellij.js
+++
@@ -1,5 +0,0 @@
-
-client.global.set("email", response.body.form.email);
-client.test("Request executed successfully", function() {
-    client.assert(response.status === 200, "Response status is not 200");
-});
--- examples/issues/#14/login.js
+++
@@ -1,7 +0,0 @@
-client.test('Token retrieved successfully', function () {
-  client.assert(response.status === 200, 'Response status is not 200');
-  var access = response.body.access_token || response.body.args.test;
-  if (access) {
-    client.global.set('user_access_token', access);
-  }
-});
