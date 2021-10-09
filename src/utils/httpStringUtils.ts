be4daf3467ac0baa88b9f2526045d3266e4fbc14
Andreas Weber <weber.andreas@gmail.com>
2021-10-09 14:18:44 +0200

    feat: options for toHttpString

--- src/utils/httpStringUtils.ts
+++ src/utils/httpStringUtils.ts
@@ -1,37 +1,51 @@
 import { HttpResponse, Request } from '../models';
 import { isString, toMultiLineString } from './stringUtils';
 
-export function toHttpString(response: HttpResponse) : string {
+export function toHttpString(response: HttpResponse, options?: {
+  responseBody?: boolean,
+  requestBody?: boolean,
+  prettyPrint?: boolean,
+}) : string {
   const result: Array<string> = [];
 
   if (response.request) {
-    result.push(...toHttpStringRequest(response.request));
+    result.push(...toHttpStringRequest(response.request, {
+      body: !!options?.requestBody
+    }));
     result.push('');
   }
-  result.push(...toHttpStringResponse(response));
+  result.push(...toHttpStringResponse(response, {
+    prettyPrint: !!options?.prettyPrint,
+    body: !!options?.responseBody
+  }));
 
   return toMultiLineString(result);
 }
 
-export function toHttpStringResponse(response: HttpResponse) : Array<string> {
+export function toHttpStringResponse(response: HttpResponse, options?: {
+  prettyPrint?: boolean;
+  body?: boolean;
+}) : Array<string> {
   const result: Array<string> = [];
   result.push(`${response.protocol} ${response.statusCode} ${response.statusMessage ? `- ${response.statusMessage}` : ''}`);
   result.push(...toHttpStringHeader(response.headers));
-  if (isString(response.body)) {
+  if (options?.body && isString(response.body)) {
     result.push('');
-    result.push(response.body);
+    result.push(options?.prettyPrint && response.prettyPrintBody ? response.prettyPrintBody : response.body);
   }
   return result;
 }
 
 
-export function toHttpStringRequest(request: Request) : Array<string> {
+export function toHttpStringRequest(request: Request, options?: {
+  body?: boolean;
+}) : Array<string> {
   const result: Array<string> = [];
   result.push(`${request.method} ${request.url}`);
   if (request.headers) {
     result.push(...toHttpStringHeader(request.headers));
   }
-  if (isString(request.body)) {
+  if (options?.body && isString(request.body)) {
     result.push('');
     result.push(request.body);
   }
