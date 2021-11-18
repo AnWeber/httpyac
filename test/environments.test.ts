a3133f1fbca2e4c8b1930158f5d893c733356137
Stafford Williams <stafford.williams@gmail.com>
2021-11-18 22:35:16 +1100

    implement test

--- test/environments.test.ts
+++ test/environments.test.ts
@@ -1,8 +1,29 @@
 import path from 'path';
 import { cli } from '../src';
+import * as httpClientFactory from '../src/io/gotHttpClientFactory';
+
 describe('environments', () => {
-  it('should pick up dev environment', async () => {
-    jest.spyOn(process, 'exit').mockImplementation();
-    await cli.execute(['', '', path.join(__dirname, '/spacex.http'), '--all', '--env', 'dev']);
+  jest.spyOn(process, 'exit').mockImplementation();
+  it('should pick up test environment', async () => {
+    const mockFactory = jest.fn();
+    jest
+      .spyOn(httpClientFactory, 'initHttpClient')
+      .mockImplementation(() => mockFactory);
+
+    await cli.execute([
+      '',
+      '',
+      path.join(__dirname, '/spacex.http'),
+      '--all',
+      '--env',
+      'test',
+    ]);
+
+    expect(mockFactory).toHaveBeenCalledWith(
+      expect.objectContaining({
+        url: 'https://api.spacexdata.com/v4/launches/latest',
+      }),
+      expect.any(Object)
+    );
   });
 });
