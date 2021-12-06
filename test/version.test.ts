775d690859e1fce3cd13cf18878dd9e3245e9560
Stafford Williams <stafford.williams@gmail.com>
2021-12-07 01:00:32 +1100

    fix version test

--- test/version.test.ts
+++ test/version.test.ts
@@ -1,17 +1,22 @@
-import { cli } from '../src';
 import * as utils from '../src/utils/configUtils';
+import { createProgram } from '@/cli';
 
 describe('version', () => {
   it('should print version', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    jest.spyOn(process, 'exit').mockImplementation(() => {});
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const mockStdout = jest.spyOn(process.stdout, 'write').mockImplementation(() => {});
     jest.spyOn(utils, 'parseJson').mockResolvedValue({ version: '1.2.3' });
-    const spy = jest.spyOn(console, 'info');
 
-    await cli.execute([
-      '',
-      '',
-      '--version'
-    ]);
+    const program = await createProgram();
 
-    expect(spy).toHaveBeenCalledWith('httpyac v1.2.3');
+    await program.parse(['--version'], { from: 'user' });
+
+    expect(mockStdout).toHaveBeenCalledWith('1.2.3\n');
   });
 });
