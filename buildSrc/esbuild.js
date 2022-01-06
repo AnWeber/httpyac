baff8e34d75861161b6493e513107a60c54f2046
Andreas Weber <weber.andreas@gmail.com>
2022-01-06 21:20:00 +0100

    build: updaet dependencies

--- buildSrc/esbuild.js
+++ buildSrc/esbuild.js
@@ -1,24 +1,27 @@
-const esbuild = require('esbuild')
+const esbuild = require('esbuild');
 
 const makeAllPackagesExternalPlugin = {
   name: 'make-all-packages-external',
   setup(build) {
-    let filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/ // Must not start with "/" or "./" or "../"
-    build.onResolve({ filter }, args => ({ path: args.path, external: true }))
+    let filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/; // Must not start with "/" or "./" or "../"
+    build.onResolve({ filter }, args => ({ path: args.path, external: true }));
   },
-}
+};
 
 const isWatchBuild = process.argv.indexOf('--watch') >= 0;
+const noMinify = process.argv.indexOf('--no-minify') >= 0;
 
-esbuild.build({
-  logLevel: "info",
-  entryPoints: ['./src/index.ts'],
-  outfile: 'dist/index.js',
-  bundle: true,
-  minify: !isWatchBuild,
-  platform: 'node',
-  sourcemap: true,
-  target: 'node14',
-  watch: isWatchBuild,
-  plugins: [makeAllPackagesExternalPlugin]
-}).catch(() => process.exit(1))
+esbuild
+  .build({
+    logLevel: 'info',
+    entryPoints: ['./src/index.ts'],
+    outfile: 'dist/index.js',
+    bundle: true,
+    minify: !noMinify,
+    platform: 'node',
+    sourcemap: true,
+    target: 'node14',
+    watch: isWatchBuild,
+    plugins: [makeAllPackagesExternalPlugin],
+  })
+  .catch(() => process.exit(1));
