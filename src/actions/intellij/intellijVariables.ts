dab7564759bea597cb96efb25bbb1c31a5d1fe85
Andreas Weber <weber.andreas@gmail.com>
2022-01-13 20:01:31 +0100

    feat: save intellij global cache in userSessionStore

--- src/actions/intellij/intellijVariables.ts
+++ src/actions/intellij/intellijVariables.ts
@@ -1,17 +1,43 @@
-import { ProcessorContext } from '../../models';
+import * as models from '../../models';
+import { userSessionStore } from '../../store';
 import * as utils from '../../utils';
-import { intellijVariableCache } from '../../variables/provider';
 import { Variables as JetbrainsVariables } from './http-client';
 
-export class IntellijVariables implements JetbrainsVariables {
-  constructor(private readonly context: ProcessorContext) {}
+interface IntellijGlobalCacheSession extends models.UserSession {
+  variables: models.Variables;
+}
 
-  private get globalCache() {
-    return intellijVariableCache[utils.toEnvironmentKey(this.context.httpFile.activeEnvironment)];
+export class IntellijVariables implements JetbrainsVariables {
+  private userSession: IntellijGlobalCacheSession;
+  constructor(private readonly context: models.ProcessorContext) {
+    this.userSession = this.getIntellijSession(context);
+  }
+
+  private getIntellijSession(context: models.ProcessorContext): IntellijGlobalCacheSession {
+    const envKey = utils.toEnvironmentKey(context.httpFile.activeEnvironment);
+    const id = `intellij_global_cache_${envKey}`;
+    const userSession = userSessionStore.getUserSession(id);
+    if (this.isIntellijGlobalCacheSession(userSession)) {
+      return userSession;
+    }
+    const intellijSession: IntellijGlobalCacheSession = {
+      id,
+      title: `Intellij Cache for ${envKey}`,
+      description: `Intellij Cache for ${envKey}`,
+      details: {},
+      type: 'intellij_global_cache',
+      variables: {},
+    };
+    userSessionStore.setUserSession(intellijSession);
+    return intellijSession;
+  }
+
+  private isIntellijGlobalCacheSession(value: models.UserSession | undefined): value is IntellijGlobalCacheSession {
+    return !!value?.id && !!value.title;
   }
 
   set(varName: string, varValue: string): void {
-    this.globalCache[varName] = varValue;
+    this.userSession.variables[varName] = varValue;
     utils.setVariableInContext(
       {
         [varName]: varValue,
@@ -23,14 +49,14 @@ export class IntellijVariables implements JetbrainsVariables {
     return this.context.variables[varName];
   }
   isEmpty(): boolean {
-    return Object.entries(this.globalCache).length === 0;
+    return Object.entries(this.userSession.variables).length === 0;
   }
   clear(varName: string): void {
-    delete this.globalCache[varName];
+    delete this.userSession.variables[varName];
     utils.deleteVariableInContext(varName, this.context);
   }
   clearAll(): void {
-    for (const [key] of Object.entries(this.globalCache)) {
+    for (const [key] of Object.entries(this.userSession.variables)) {
       this.clear(key);
     }
   }
--- src/variables/provider/intellijGlobalVariableProvider.ts
+++ src/variables/provider/intellijGlobalVariableProvider.ts
@@ -1,12 +1,10 @@
-import { Variables } from '../../models';
+import * as models from '../../models';
+import { userSessionStore } from '../../store';
 import { toEnvironmentKey } from '../../utils';
 
-export const intellijVariableCache: Record<string, Variables> = {};
-
-export async function provideIntellijGlobalVariables(env: string[] | undefined): Promise<Variables> {
-  const envkey = toEnvironmentKey(env);
-  if (!intellijVariableCache[envkey]) {
-    intellijVariableCache[envkey] = {};
-  }
-  return intellijVariableCache[envkey];
+export async function provideIntellijGlobalVariables(env: string[] | undefined): Promise<models.Variables> {
+  const envKey = toEnvironmentKey(env);
+  const userSession: (models.UserSession & { variables?: models.Variables }) | undefined =
+    userSessionStore.getUserSession(`intellij_global_cache_${envKey}`);
+  return userSession?.variables || {};
 }
