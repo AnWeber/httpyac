d10bdf7deec2c050f12113c5ad60a181b178673e
Andreas Weber <weber.andreas@gmail.com>
2022-01-27 21:38:36 +0100

    refactor: move host header logic to hostVariableReplacer

--- src/variables/replacer/hostVariableReplacer.ts
+++ src/variables/replacer/hostVariableReplacer.ts
@@ -1,14 +1,22 @@
 import { ProcessorContext, VariableType } from '../../models';
-import { isString } from '../../utils';
+import * as utils from '../../utils';
 
 export async function hostVariableReplacer(
   text: unknown,
   type: VariableType | string,
-  { variables }: ProcessorContext
+  { variables, request }: ProcessorContext
 ): Promise<unknown> {
-  if (isString(text) && VariableType.url === type && !!variables.host) {
+  if (utils.isString(text) && VariableType.url === type) {
     if (text.startsWith('/')) {
-      return `${variables.host}${text}`;
+      if (variables.host) {
+        return `${variables.host}${text}`;
+      }
+      const host = utils.getHeader(request?.headers, 'Host');
+      if (utils.isString(host)) {
+        const [, port] = host.toString().split(':');
+        const scheme = port === '443' || port === '8443' ? 'https' : 'http';
+        return `${scheme}://${host}${text}`;
+      }
     }
   }
   return text;
