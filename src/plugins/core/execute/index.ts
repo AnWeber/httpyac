587a48278f3a4936443e44c9889473d187e15ce6
Andreas Weber <weber.andreas@gmail.com>
2022-02-15 22:05:42 +0100

    refactor: move cookieJarInterceptor to http plugin

--- src/plugins/core/execute/index.ts
+++ src/plugins/core/execute/index.ts
@@ -1,8 +1,6 @@
 import * as models from '../../../models';
-import { CookieJarInterceptor } from './cookieJarInterceptor';
 import { CreateRequestInterceptor } from './createRequestInterceptor';
 
 export function initExecuteInterceptor(api: models.HttpyacHooksApi) {
   api.hooks.execute.addInterceptor(new CreateRequestInterceptor());
-  api.hooks.execute.addInterceptor(new CookieJarInterceptor());
 }
--- src/plugins/http/index.ts
+++ src/plugins/http/index.ts
@@ -1,10 +1,12 @@
 import { httpClientProvider } from '../../io';
 import * as models from '../../models';
+import { CookieJarInterceptor } from './cookieJarInterceptor';
 import { digestAuthVariableReplacer } from './digestAuthVariableReplacer';
 import { gotHttpClient } from './gotUtils';
 
 httpClientProvider.exchange = gotHttpClient;
 
 export function registerHttpPlugin(api: models.HttpyacHooksApi) {
+  api.hooks.execute.addInterceptor(new CookieJarInterceptor());
   api.hooks.replaceVariable.addHook('digestAuth', digestAuthVariableReplacer);
 }
