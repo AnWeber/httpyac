c732e9c54d9e0ccce7ad256f6c06d8e7a913ac3f
Andreas Weber <weber.andreas@gmail.com>
2022-02-28 19:42:37 +0100

    feat: add test for attachDefaultHeaders

---
+++ src/plugins/core/request/__tests__/attachDefaultHeaders.test.ts
@@ -0,0 +1,30 @@
+import * as models from '../../../../models';
+import { attachDefaultHeaders } from '../attachDefaultHeaders';
+
+describe('attachDefaultHeaders', () => {
+  describe('attachDefaultHeaders', () => {
+    it('set request headers if no headers are specified', async () => {
+      const request: models.Request = {};
+      await attachDefaultHeaders(request, {
+        config: {
+          defaultHeaders: {
+            foo: 'bar',
+          },
+        },
+      } as unknown as models.ProcessorContext);
+      expect(request.headers?.foo).toEqual('bar');
+    });
+    it('add request headers to defined request headers', async () => {
+      const request: models.Request = { headers: { bar: 'foo' } };
+      await attachDefaultHeaders(request, {
+        config: {
+          defaultHeaders: {
+            foo: 'bar',
+          },
+        },
+      } as unknown as models.ProcessorContext);
+      expect(request.headers?.foo).toEqual('bar');
+      expect(request.headers?.bar).toEqual('foo');
+    });
+  });
+});
