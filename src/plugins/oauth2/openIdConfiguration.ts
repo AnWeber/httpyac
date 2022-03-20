5facd8f2e3f643f4feaeeb1ab4d3fa2c5eea32de
Andreas Weber <weber.andreas@gmail.com>
2022-03-20 22:04:59 +0100

    refactor: better readability for getOpenIdConfiguration

--- src/plugins/oauth2/openIdConfiguration.ts
+++ src/plugins/oauth2/openIdConfiguration.ts
@@ -29,59 +29,48 @@ function getVariableUnknown(variables: models.Variables, variablePrefix: string 
   return expandedValue;
 }
 
-function getString(variables: models.Variables, variablePrefix: string | undefined, name: string): string {
-  const expandedValue = getVariableUnknown(variables, variablePrefix, name);
-  return utils.ensureString(expandedValue);
-}
-
-function getBooleanLike(
-  variables: models.Variables,
-  variablePrefix: string | undefined,
-  name: string,
-  defaultValue = false
-): boolean {
-  const expandedValue = getVariableUnknown(variables, variablePrefix, name);
-  return utils.toBoolean(expandedValue, defaultValue);
-}
-
-function getUrl(
-  variables: models.Variables,
-  variablePrefix: string | undefined,
-  name: string,
-  defaultUrl: string
-): URL {
-  const url = getString(variables, variablePrefix, name);
-  try {
-    return new URL(url || defaultUrl);
-  } catch {
-    throw new Error(`Expected a valid URL, but received ${url}`);
-  }
-}
-
 export function getOpenIdConfiguration(
   variablePrefix: string | undefined,
   variables: models.Variables
 ): models.OpenIdConfiguration {
+  const getString = (name: string) => {
+    const expandedValue = getVariableUnknown(variables, variablePrefix, name);
+    return utils.ensureString(expandedValue);
+  };
+
+  const getUrl = (name: string, defaultUrl: string) => {
+    const url = utils.ensureString(getVariableUnknown(variables, variablePrefix, name));
+    try {
+      return new URL(url || defaultUrl);
+    } catch {
+      throw new Error(`Expected a valid URL, but received ${url}`);
+    }
+  };
+  const getBoolean = (name: string, defaultValue = false) => {
+    const expandedValue = getVariableUnknown(variables, variablePrefix, name);
+    return utils.toBoolean(expandedValue, defaultValue);
+  };
+
   const config: models.OpenIdConfiguration = {
     variablePrefix: variablePrefix || DefaultOAuthVariablePrefix,
-    authorizationEndpoint: getString(variables, variablePrefix, 'authorizationEndpoint'),
-    deviceCodeEndpoint: getString(variables, variablePrefix, 'deviceCodeEndpoint'),
-    tokenEndpoint: getString(variables, variablePrefix, 'tokenEndpoint'),
-    clientId: getString(variables, variablePrefix, 'clientId'),
-    clientSecret: getString(variables, variablePrefix, 'clientSecret'),
-    responseType: getString(variables, variablePrefix, 'responseType'),
-    responseMode: getString(variables, variablePrefix, 'responseMode'),
-    audience: getString(variables, variablePrefix, 'audience'),
-    scope: getString(variables, variablePrefix, 'scope'),
-    resource: getString(variables, variablePrefix, 'resource'),
-    username: getString(variables, variablePrefix, 'username'),
-    password: getString(variables, variablePrefix, 'password'),
-    subjectIssuer: getString(variables, variablePrefix, 'subjectIssuer'),
-    redirectUri: getUrl(variables, variablePrefix, 'redirectUri', DEFAULT_CALLBACK_URI),
-    keepAlive: getBooleanLike(variables, variablePrefix, 'keepAlive', true),
-    useAuthorizationHeader: getBooleanLike(variables, variablePrefix, 'useAuthorizationHeader', true),
-    useDeviceCodeClientSecret: getBooleanLike(variables, variablePrefix, 'useDeviceCodeClientSecret'),
-    usePkce: getBooleanLike(variables, variablePrefix, 'usePkce'),
+    authorizationEndpoint: getString('authorizationEndpoint'),
+    deviceCodeEndpoint: getString('deviceCodeEndpoint'),
+    tokenEndpoint: getString('tokenEndpoint'),
+    clientId: getString('clientId'),
+    clientSecret: getString('clientSecret'),
+    responseType: getString('responseType'),
+    responseMode: getString('responseMode'),
+    audience: getString('audience'),
+    scope: getString('scope'),
+    resource: getString('resource'),
+    username: getString('username'),
+    password: getString('password'),
+    subjectIssuer: getString('subjectIssuer'),
+    redirectUri: getUrl('redirectUri', DEFAULT_CALLBACK_URI),
+    keepAlive: getBoolean('keepAlive', true),
+    useAuthorizationHeader: getBoolean('useAuthorizationHeader', true),
+    useDeviceCodeClientSecret: getBoolean('useDeviceCodeClientSecret'),
+    usePkce: getBoolean('usePkce'),
   };
   return config;
 }
