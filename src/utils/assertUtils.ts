23836c2f4512e72f7f05be1cf3c3f2af35302eb1
Andreas Weber <weber.andreas@gmail.com>
2022-04-01 21:22:52 +0200

    feat: use custom assert instead of console.assert

--- src/utils/assertUtils.ts
+++ src/utils/assertUtils.ts
@@ -2,14 +2,20 @@ import { HttpResponse } from '../models';
 import { getHeader } from './requestUtils';
 import { isString } from './stringUtils';
 
+function assert(condition: boolean, message: string) {
+  if (!condition) {
+    throw new Error(message);
+  }
+}
+
 export function assertStatusEquals(response: HttpResponse, status: number): void {
-  console.assert(response.statusCode === status, `response status equals to ${status}`);
+  assert(response.statusCode === status, `response status is ${response.statusCode} (expected: ${status})`);
 }
 
 export function assertMaxTotalTime(response: HttpResponse, maxTotalTime: number): void {
-  console.assert(
+  assert(
     response.timings?.total ? response.timings.total < maxTotalTime : true,
-    `total time exceeded ${maxTotalTime}`
+    `total time is ${response.timings?.total} (expected max ${maxTotalTime})`
   );
 }
 
@@ -19,19 +25,13 @@ export function assertHeaderEquals(
   val: string | string[] | undefined
 ): void {
   const headerValue = getHeader(response.headers, headerKey);
-  console.assert(headerValue === val, `response header equals ${val}`);
+  assert(headerValue === val, `response header ${headerKey} is ${headerValue} (expected: ${val})`);
 }
 
 export function assertHeaderContains(response: HttpResponse, headerKey: string, val: string): void {
   const headerValue = getHeader(response.headers, headerKey);
   if (isString(headerValue) || Array.isArray(headerValue)) {
-    console.assert(!!headerValue.indexOf(val), `response header contains ${val}`);
-  }
-}
-
-export function assertResponseTimeLower(response: HttpResponse, maxTotal: number): void {
-  if (response.timings?.total) {
-    console.assert(response.timings.total <= maxTotal, `response time lower ${maxTotal}`);
+    assert(!!headerValue.indexOf(val), `response header contains ${val}`);
   }
 }
 
@@ -39,9 +39,9 @@ export function assertHasResponseBody(response: HttpResponse): void {
   console.assert(!!response.body, 'response body exists');
 }
 export function assertHasNoResponseBody(response: HttpResponse): void {
-  console.assert(!response.body, 'response body does not exists');
+  assert(!response.body, 'response body does not exists');
 }
 
 export function assertResponseBodyEquals(response: HttpResponse, val: unknown): void {
-  console.assert(response.body === val, `response body equals ${val}`);
+  assert(response.body === val, `response body equals ${val}`);
 }
