91bf84411b88b206bc7c95ee1e805dfe7da84069
Andreas Weber <weber.andreas@gmail.com>
2022-04-04 20:14:39 +0200

    feat: add uuid and days to javascript require #241

--- src/plugins/javascript/index.ts
+++ src/plugins/javascript/index.ts
@@ -3,6 +3,8 @@ import * as models from '../../models';
 import { parseJavascript } from './javascriptHttpRegionParser';
 import { replaceJavascriptExpressions } from './javascriptVariableReplacer';
 import * as moduleUtils from './moduleUtils';
+import { default as dayjs } from 'dayjs';
+import * as uuid from 'uuid';
 
 // register early to allow using imported Javascript Plugins
 httpyac.io.javascriptProvider.loadModule = moduleUtils.loadModule;
@@ -11,7 +13,15 @@ export function registerJavascriptPlugin(api: models.HttpyacHooksApi) {
   api.hooks.replaceVariable.addHook('javascript', replaceJavascriptExpressions, { before: ['aws'] });
   api.hooks.parse.addHook('javascript', parseJavascript, { before: ['request'] });
 
-  httpyac.io.javascriptProvider.require.httpyac = httpyac;
+  addDefaultRequire();
   httpyac.io.javascriptProvider.evalExpression = moduleUtils.evalExpression;
   httpyac.io.javascriptProvider.runScript = moduleUtils.runScript;
+}
+
+function addDefaultRequire() {
+  Object.assign(httpyac.io.javascriptProvider.require, {
+    httpyac,
+    dayjs,
+    uuid,
+  });
 }
