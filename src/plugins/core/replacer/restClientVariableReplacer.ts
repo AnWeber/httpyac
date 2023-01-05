baac88cbfd8e39c2f5f4b4fff6998ac21fde842b
Andreas Weber <weber.andreas@gmail.com>
2023-01-05 21:54:49 +0100

    feat: add support for more intellij dynamic variables AnWeber/vscode-httpyac#165

--- src/plugins/core/replacer/restClientVariableReplacer.ts
+++ src/plugins/core/replacer/restClientVariableReplacer.ts
@@ -19,9 +19,9 @@ export async function restClientVariableReplacer(
     if (trimmedVariable.startsWith('$randomInt')) {
       const valMatch = /^\$randomInt\s*(?<min>-?\d+)?\s*(?<max>-?\d+)?\s*$/u.exec(trimmedVariable);
       if (valMatch && valMatch.groups?.min && valMatch.groups?.max) {
-        let min = utils.toNumber(valMatch.groups?.min);
-        let max = utils.toNumber(valMatch.groups?.max);
-        if (min && max) {
+        let min = utils.toNumber(valMatch.groups?.min) || 0;
+        let max = utils.toNumber(valMatch.groups?.max) || 100;
+        if (!Number.isNaN(min) && !Number.isNaN(max)) {
           if (min > max) {
             const temp = max;
             max = min;
--- src/plugins/intellij/intellijVariableReplacer.ts
+++ src/plugins/intellij/intellijVariableReplacer.ts
@@ -3,15 +3,112 @@ import { v4 } from 'uuid';
 
 export async function replaceDynamicIntellijVariables(text: unknown): Promise<unknown> {
   return utils.parseHandlebarsString(text, async (variable: string) => {
-    switch (variable.trim()) {
-      case '$uuid':
-        return v4();
-      case '$timestamp':
-        return Date.now();
-      case '$randomInt':
-        return Math.floor(Math.random() * 1000);
-      default:
-        return undefined;
+    const trimmedVariable = variable.trim();
+    if (['$uuid', '$random.uuid'].indexOf(trimmedVariable) >= 0) {
+      return v4();
     }
+    if (trimmedVariable === '$timestamp') {
+      return Date.now();
+    }
+    if (trimmedVariable === '$isoTimestamp') {
+      return new Date().toISOString();
+    }
+    if (trimmedVariable === '$randomInt') {
+      return Math.floor(Math.random() * 1000);
+    }
+    if (trimmedVariable.startsWith('$random.integer')) {
+      const float = randomFloat(trimmedVariable);
+      if (float) {
+        return `${Math.floor(float)}`;
+      }
+    }
+    if (trimmedVariable.startsWith('$random.float')) {
+      const float = randomFloat(trimmedVariable);
+      if (float) {
+        return `${float}`;
+      }
+    }
+    if (trimmedVariable.startsWith('$random.alphabetic')) {
+      return randomAlphabetic(trimmedVariable);
+    }
+    if (trimmedVariable.startsWith('$random.hexadecimal')) {
+      return randomHexadecimal(trimmedVariable);
+    }
+
+    if (trimmedVariable.startsWith('$random.email')) {
+      return `${randomText(30)}@${randomText(10)}.${randomArrayValue([
+        'com',
+        'org',
+        'at',
+        'de',
+        'fr',
+        'uk',
+        'it',
+        'ch',
+        'info',
+        'edu',
+        'asia',
+        'gov',
+        'app',
+        'io',
+      ])}`;
+    }
+
+    return undefined;
   });
+}
+
+function randomFloat(variable: string) {
+  const match = /^\$random.(integer|float)\s*\(\s*(?<from>-?\d+)\s*,\s*(?<to>-?\d+)\s*\)$/u.exec(variable);
+  if (match && match.groups?.from && match.groups?.to) {
+    let from = utils.toNumber(match.groups?.from) || 0;
+    let to = utils.toNumber(match.groups?.to) || 0;
+    if (!Number.isNaN(from) && !Number.isNaN(to)) {
+      if (from > to) {
+        const temp = to;
+        to = from;
+        from = temp;
+      }
+      return Math.random() * (to - from) + from;
+    }
+  }
+  return undefined;
+}
+
+function randomAlphabetic(variable: string) {
+  const match = /^\$random.alphabetic\s*\(\s*(?<length>-?\d+)\s*\)\s*$/u.exec(variable);
+  if (match && match.groups?.length) {
+    const length = utils.toNumber(match.groups?.length);
+    if (length) {
+      return randomText(length);
+    }
+  }
+  return undefined;
+}
+function randomHexadecimal(variable: string) {
+  const match = /^\$random.hexadecimal\s*\(\s*(?<length>-?\d+)\s*\)\s*$/u.exec(variable);
+  if (match && match.groups?.length) {
+    const length = utils.toNumber(match.groups?.length);
+    if (length) {
+      return randomText(length, '1234567890ABCDEF');
+    }
+  }
+  return undefined;
+}
+
+function randomText(length: number, chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz') {
+  const result = [];
+
+  if (length > 0) {
+    const abc = chars.split('');
+    for (let index = 0; index < length; index++) {
+      result.push(randomArrayValue(abc));
+    }
+  }
+
+  return result.join('');
+}
+
+function randomArrayValue(values: Array<unknown>) {
+  return values[Math.floor(Math.random() * (values.length - 1))];
 }
