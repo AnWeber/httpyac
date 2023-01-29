a5807d4d727d26ad9834c33aea483318bc605d26
Andreas Weber <weber.andreas@gmail.com>
2023-01-29 19:26:51 +0100

    refactor: extract generic requestParser

--- src/plugins/rabbitmq/amqpHttpRegionParser.ts
+++ src/plugins/rabbitmq/amqpHttpRegionParser.ts
@@ -1,137 +1,18 @@
-import * as models from '../../models';
 import * as utils from '../../utils';
 import { getAmqpMethod } from './amqpMethods/amqpConstants';
-import { AmqpRequest } from './amqpRequest';
+import { isAmqpRequest } from './amqpRequest';
 import { AmqpRequestClient } from './amqpRequestClient';
 
-const AmqpLine = /^\s*(amqp)\s*(?<url>.+?)\s*$/iu;
-const AmqpProtocol = /^\s*amqp(s)?:\/\/(?<url>.+?)\s*$/iu;
-
-export async function parseAmqpLine(
-  getLineReader: models.getHttpLineGenerator,
-  context: models.ParserContext
-): Promise<models.HttpRegionParserResult> {
-  const lineReader = getLineReader();
-  const next = lineReader.next();
-  if (!next.done && isValidAmqp(next.value.textLine, context.httpRegion)) {
-    if (context.httpRegion.request) {
-      return {
-        endRegionLine: next.value.line - 1,
-        nextParserLine: next.value.line - 1,
-        symbols: [],
-      };
+export const parseAmqpLine = utils.parseRequestLineFactory({
+  protocol: 'AMQP',
+  methodRegex: /^\s*(amqp)\s*(?<url>.+?)\s*$/iu,
+  protocolRegex: /^\s*amqp(s)?:\/\/(?<url>.+?)\s*$/iu,
+  requestClientFactory(request, context) {
+    return new AmqpRequestClient(request, context);
+  },
+  modifyRequest(request) {
+    if (isAmqpRequest(request)) {
+      request.method = getAmqpMethod(request);
     }
-
-    const requestLine = getAmqpLine(next.value.textLine, next.value.line);
-    if (!requestLine) {
-      return false;
-    }
-    context.httpRegion.request = requestLine.request;
-    const requestSymbol: models.HttpSymbol = {
-      name: next.value.textLine,
-      description: 'amqp request-line',
-      kind: models.HttpSymbolKind.requestLine,
-      startLine: next.value.line,
-      startOffset: 0,
-      endLine: next.value.line,
-      endOffset: next.value.textLine.length,
-      children: [requestLine.symbol],
-    };
-
-    const result: models.HttpRegionParserResult = {
-      nextParserLine: next.value.line,
-      symbols: [requestSymbol],
-    };
-
-    const headers = {};
-    requestLine.request.headers = headers;
-
-    const headersResult = await utils.parseSubsequentLines(
-      lineReader,
-      [
-        utils.parseComments,
-        utils.parseRequestHeaderFactory(headers),
-        utils.parseDefaultHeadersFactory(),
-        utils.parseUrlLineFactory(url => (requestLine.request.url += url)),
-      ],
-      context
-    );
-
-    if (headersResult) {
-      result.nextParserLine = headersResult.nextLine || result.nextParserLine;
-      for (const parseResult of headersResult.parseResults) {
-        result.symbols?.push?.(...parseResult.symbols);
-      }
-    }
-
-    requestLine.request.method = getAmqpMethod(requestLine.request);
-
-    context.httpRegion.hooks.execute.addHook(
-      'amqp',
-      utils.executeRequestClientFactory((request, context) => new AmqpRequestClient(request, context))
-    );
-
-    return result;
-  }
-  return false;
-}
-
-function getAmqpLine(textLine: string, line: number): { request: AmqpRequest; symbol: models.HttpSymbol } | undefined {
-  const lineMatch = AmqpLine.exec(textLine);
-  if (lineMatch && lineMatch.length > 1 && lineMatch.groups) {
-    return {
-      request: {
-        supportsStreaming: true,
-        url: lineMatch.groups.url,
-        protocol: 'AMQP',
-        method: 'AMQP',
-      },
-      symbol: {
-        name: lineMatch.groups.url,
-        description: 'amqp url',
-        kind: models.HttpSymbolKind.url,
-        startLine: line,
-        startOffset: 0,
-        endLine: line,
-        endOffset: textLine.length,
-        children: utils.parseHandlebarsSymbols(textLine, line),
-      },
-    };
-  }
-  const protocolMatch = AmqpProtocol.exec(textLine);
-  if (protocolMatch && protocolMatch.length > 1 && protocolMatch.groups) {
-    return {
-      request: {
-        supportsStreaming: true,
-        url: protocolMatch.groups.url,
-        protocol: 'AMQP',
-        method: 'AMQP',
-      },
-      symbol: {
-        name: protocolMatch.groups.url,
-        description: 'amqp url',
-        kind: models.HttpSymbolKind.url,
-        startLine: line,
-        startOffset: 0,
-        endLine: line,
-        endOffset: textLine.length,
-        children: utils.parseHandlebarsSymbols(textLine, line),
-      },
-    };
-  }
-  return undefined;
-}
-
-function isValidAmqp(textLine: string, httpRegion: models.HttpRegion) {
-  if (utils.isStringEmpty(textLine)) {
-    return false;
-  }
-
-  if (AmqpLine.exec(textLine)?.groups?.url) {
-    return true;
-  }
-  if (!httpRegion.request) {
-    return AmqpProtocol.exec(textLine)?.groups?.url;
-  }
-  return false;
-}
+  },
+});
---
+++ src/utils/parseRequestClientFactory.ts
@@ -0,0 +1,145 @@
+import * as models from '../models';
+import * as parser from './parserUtils';
+import { executeRequestClientFactory } from './requestClientUtils';
+import { isStringEmpty } from './stringUtils';
+
+interface RequestParserContext {
+  protocol: string;
+  method?: string;
+  requestClientFactory: (request: models.Request, context: models.ProcessorContext) => models.RequestClient;
+  modifyRequest?: (request: models.Request) => void;
+  methodRegex: RegExp;
+  protocolRegex?: RegExp;
+}
+
+export function parseRequestLineFactory(requestContext: RequestParserContext) {
+  return async function parseRequestLine(
+    getLineReader: models.getHttpLineGenerator,
+    context: models.ParserContext
+  ): Promise<models.HttpRegionParserResult> {
+    const lineReader = getLineReader();
+    const next = lineReader.next();
+    if (!next.done && isValidRequestLine(next.value, context.httpRegion, requestContext)) {
+      if (context.httpRegion.request) {
+        return {
+          endRegionLine: next.value.line - 1,
+          nextParserLine: next.value.line - 1,
+          symbols: [],
+        };
+      }
+
+      const requestLine = getRequestParseLine(next.value, requestContext);
+      if (!requestLine) {
+        return false;
+      }
+      context.httpRegion.request = requestLine.request;
+      const requestSymbol: models.HttpSymbol = {
+        name: next.value.textLine,
+        description: `${requestContext.protocol} request-line`,
+        kind: models.HttpSymbolKind.requestLine,
+        startLine: next.value.line,
+        startOffset: 0,
+        endLine: next.value.line,
+        endOffset: next.value.textLine.length,
+        children: [requestLine.symbol],
+      };
+
+      const result: models.HttpRegionParserResult = {
+        nextParserLine: next.value.line,
+        symbols: [requestSymbol],
+      };
+
+      const headers = {};
+      requestLine.request.headers = headers;
+
+      const headersResult = await parser.parseSubsequentLines(
+        lineReader,
+        [
+          parser.parseComments,
+          parser.parseRequestHeaderFactory(headers),
+          parser.parseDefaultHeadersFactory(),
+          parser.parseUrlLineFactory(url => (requestLine.request.url += url)),
+        ],
+        context
+      );
+
+      if (headersResult) {
+        result.nextParserLine = headersResult.nextLine || result.nextParserLine;
+        for (const parseResult of headersResult.parseResults) {
+          result.symbols?.push?.(...parseResult.symbols);
+        }
+      }
+      requestContext.modifyRequest?.(requestLine.request);
+
+      context.httpRegion.hooks.execute.addHook(
+        requestContext.protocol,
+        executeRequestClientFactory(requestContext.requestClientFactory)
+      );
+
+      return result;
+    }
+    return false;
+  };
+}
+function getRequestParseLine(
+  httpLine: models.HttpLine,
+  context: RequestParserContext
+): { request: models.Request; symbol: models.HttpSymbol } | undefined {
+  const lineMatch = context.methodRegex.exec(httpLine.textLine);
+  if (lineMatch && lineMatch.length > 1 && lineMatch.groups) {
+    return {
+      request: {
+        supportsStreaming: true,
+        url: lineMatch.groups.url,
+        protocol: context.protocol.toUpperCase(),
+        method: lineMatch.groups?.method || context.method || context.protocol,
+      },
+      symbol: {
+        name: lineMatch.groups.url,
+        description: `${context.protocol} Url`,
+        kind: models.HttpSymbolKind.url,
+        startLine: httpLine.line,
+        startOffset: 0,
+        endLine: httpLine.line,
+        endOffset: httpLine.textLine.length,
+        children: parser.parseHandlebarsSymbols(httpLine.textLine, httpLine.line),
+      },
+    };
+  }
+  const protocolMatch = context.protocolRegex?.exec(httpLine.textLine);
+  if (protocolMatch && protocolMatch.length > 1 && protocolMatch.groups) {
+    return {
+      request: {
+        supportsStreaming: true,
+        url: protocolMatch.groups.url,
+        protocol: context.protocol.toUpperCase(),
+        method: protocolMatch.groups?.method || context.method || context.protocol,
+      },
+      symbol: {
+        name: protocolMatch.groups.url,
+        description: `${context.protocol} Url`,
+        kind: models.HttpSymbolKind.url,
+        startLine: httpLine.line,
+        startOffset: 0,
+        endLine: httpLine.line,
+        endOffset: httpLine.textLine.length,
+        children: parser.parseHandlebarsSymbols(httpLine.textLine, httpLine.line),
+      },
+    };
+  }
+  return undefined;
+}
+
+function isValidRequestLine(httpLine: models.HttpLine, httpRegion: models.HttpRegion, context: RequestParserContext) {
+  if (isStringEmpty(httpLine.textLine)) {
+    return false;
+  }
+
+  if (context.methodRegex.exec(httpLine.textLine)?.groups?.url) {
+    return true;
+  }
+  if (!httpRegion.request && context.protocolRegex) {
+    return context.protocolRegex.exec(httpLine.textLine)?.groups?.url;
+  }
+  return false;
+}
