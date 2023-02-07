742f8cbc1565373b85598b4771060877819ddb9a
Andreas Weber <weber.andreas@gmail.com>
2023-02-07 21:45:19 +0100

    feat: add test for body without empty line

--- src/test/request/body.spec.ts
+++ src/test/request/body.spec.ts
@@ -5,8 +5,38 @@ describe('request.body', () => {
   const localServer = getLocal();
   beforeEach(() => localServer.start(7001));
   afterEach(() => localServer.stop());
+  it('should send body', async () => {
+    initFileProvider();
+    const body = JSON.stringify({ foo: 'foo', bar: 'bar' }, null, 2);
+    const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
 
-  it('imported body', async () => {
+    await sendHttp(`
+POST http://localhost:7001/post
+Content-Type: application/json
+
+${body}
+    `);
+
+    const requests = await mockedEndpoints.getSeenRequests();
+    expect(requests[0].headers['content-type']).toBe('application/json');
+    expect(await requests[0].body.getText()).toBe(body);
+  });
+  it('should send body with no empty line', async () => {
+    initFileProvider();
+    const body = JSON.stringify({ foo: 'foo', bar: 'bar' }, null, 2);
+    const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
+
+    await sendHttp(`
+POST http://localhost:7001/post
+Content-Type: application/json
+${body}
+    `);
+
+    const requests = await mockedEndpoints.getSeenRequests();
+    expect(requests[0].headers['content-type']).toBe('application/json');
+    expect(await requests[0].body.getText()).toBe(body);
+  });
+  it('should send imported body', async () => {
     const body = JSON.stringify({ foo: 'foo', bar: 'bar' }, null, 2);
     initFileProvider({ 'body.json': body });
     const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
@@ -23,7 +53,7 @@ Content-Type: application/json
     expect(await requests[0].body.getText()).toBe(body);
   });
 
-  it('imported buffer body', async () => {
+  it('should send imported buffer body', async () => {
     const body = JSON.stringify({ foo: 'foo', bar: 'bar' }, null, 2);
     initFileProvider({ 'body.json': body });
     const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
@@ -39,7 +69,7 @@ Content-Type: application/json
     expect(requests[0].headers['content-type']).toBe('application/json');
     expect(await requests[0].body.getText()).toBe(body);
   });
-  it('imported buffer body with replace', async () => {
+  it('should send imported buffer body with replace', async () => {
     const body = JSON.stringify({ foo: 'foo', bar: '{{bar}}' }, null, 2);
     initFileProvider({ 'body.json': body });
     const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
