37c3bfba9c62d2e0a7d9ccc1b1729c8e06c41042
Andreas Weber <weber.andreas@gmail.com>
2023-02-12 16:43:19 +0100

    feat: add promisequeue utiltity AnWeber/vscode-httpyac#177

--- src/utils/promiseUtils.ts
+++ src/utils/promiseUtils.ts
@@ -6,3 +6,20 @@ export function isPromise(obj: unknown): obj is Promise<unknown> {
 export function sleep(timeout: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, timeout));
 }
+
+export async function promiseQueue<T>(batchSize: number, ...promises: Array<() => Promise<T>>) {
+  const result: Array<T> = [];
+  const queue = async () => {
+    let promise: (() => Promise<T>) | undefined;
+    while ((promise = promises.pop())) {
+      result.push(await promise());
+    }
+  };
+
+  const queuePromises: Array<Promise<unknown>> = [];
+  for (let index = 0; index < batchSize; index++) {
+    queuePromises.push(queue());
+  }
+  await Promise.all(queuePromises);
+  return result;
+}
