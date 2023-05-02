93444e5d1c02c34266a07496679202e4902854cc
Andreas Weber <weber.andreas@gmail.com>
2023-05-02 20:57:57 +0200

    ci: update endpoints

--- src/test/metaData/import.spec.ts
+++ src/test/metaData/import.spec.ts
@@ -15,13 +15,13 @@ GET  /json
       `,
     });
     await localServer.forGet('/json').thenJson(200, { foo: 'bar', test: 1 });
-    const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
+    const mockedEndpoints = await localServer.forPost('/nameimport').thenReply(200);
     const httpFile = await parseHttp(
       `
 # @import ./import.http
 ###
 # @ref foo
-POST /post?test={{foo.test}}
+POST /nameimport?test={{foo.test}}
 
 foo={{foo.foo}}
     `
@@ -36,7 +36,7 @@ foo={{foo.foo}}
     });
 
     const requests = await mockedEndpoints.getSeenRequests();
-    expect(requests[0].url).toBe(`http://localhost:${localServer.port}/post?test=1`);
+    expect(requests[0].path).toBe(`/nameimport?test=1`);
     expect(await requests[0].body.getText()).toBe('foo=bar');
   });
   it('import global variable', async () => {
@@ -47,10 +47,10 @@ foo={{foo.foo}}
       `,
     });
     await localServer.forGet('/json').thenJson(200, { foo: 'bar', test: 1 });
-    const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
+    const mockedEndpoints = await localServer.forPost('/globalvarimport').thenReply(200);
     const httpFile = await parseHttp(`
 # @import ./import.http
-POST /post?foo={{foo}}
+POST /globalvarimport?foo={{foo}}
 
 bar={{bar}}
     `);
@@ -64,7 +64,7 @@ bar={{bar}}
     });
 
     const requests = await mockedEndpoints.getSeenRequests();
-    expect(requests[0].url).toBe(`http://localhost:${localServer.port}/post?foo=bar`);
+    expect(requests[0].path).toBe(`/globalvarimport?foo=bar`);
     expect(await requests[0].body.getText()).toBe('bar=foo');
   });
   it('import named variable', async () => {
@@ -77,11 +77,11 @@ bar={{bar}}
       `,
     });
     await localServer.forGet('/json').thenJson(200, { foo: 'bar', test: 1 });
-    const mockedEndpoints = await localServer.forPost('/post').thenReply(200);
+    const mockedEndpoints = await localServer.forPost('/namedvar').thenReply(200);
     const httpFile = await parseHttp(`
 # @import ./import.http
 # @ref test
-POST /post?foo={{foo}}
+POST /namedvar?foo={{foo}}
 
 bar={{bar}}
     `);
@@ -95,7 +95,7 @@ bar={{bar}}
     });
 
     const requests = await mockedEndpoints.getSeenRequests();
-    expect(requests[0].url).toBe(`http://localhost:${localServer.port}/post?foo=bar`);
+    expect(requests[0].path).toBe(`/namedvar?foo=bar`);
     expect(await requests[0].body.getText()).toBe('bar=foo');
   });
   it('import request with using global host variable', async () => {
@@ -105,7 +105,7 @@ bar={{bar}}
 
       ### Apple
       # @name send_apple
-      POST /anything
+      POST /globalhostimport
       Content-Type: application/json
 
       {
@@ -115,8 +115,8 @@ bar={{bar}}
       }
       `,
     });
-    await localServer.forGet('/json').thenJson(200, { foo: 'bar', test: 1 });
-    const mockedEndpoints = await localServer.forPost('/anything').thenReply(200);
+    await localServer.forGet('/globalhost').thenJson(200, { foo: 'bar', test: 1 });
+    const mockedEndpoints = await localServer.forPost('/globalhostimport').thenReply(200);
     const httpFile = await parseHttp(`
 # @import ./import.http
 
@@ -130,7 +130,7 @@ bar={{bar}}
     });
 
     const requests = await mockedEndpoints.getSeenRequests();
-    expect(requests[0].url).toBe(`http://localhost:${localServer.port}/anything`);
+    expect(requests[0].path).toBe(`/globalhostimport`);
     expect(requests[0].headers['content-type']).toBe('application/json');
   });
 });
