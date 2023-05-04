e989e42b1cd9152efaae6f58dc1134494decd2ba
Andreas Weber <weber.andreas@gmail.com>
2023-05-04 20:10:58 +0200

    fix: type issue in test

--- src/plugins/core/request/attachDefaultHeaders.spec.ts
+++ src/plugins/core/request/attachDefaultHeaders.spec.ts
@@ -4,7 +4,7 @@ import { attachDefaultHeaders } from './attachDefaultHeaders';
 describe('attachDefaultHeaders', () => {
   describe('attachDefaultHeaders', () => {
     it('set request headers if no headers are specified', async () => {
-      const request: models.Request = {};
+      const request: models.Request = { url: 'foo' };
       await attachDefaultHeaders(request, {
         config: {
           defaultHeaders: {
@@ -15,7 +15,7 @@ describe('attachDefaultHeaders', () => {
       expect(request.headers?.foo).toEqual('bar');
     });
     it('add request headers to defined request headers', async () => {
-      const request: models.Request = { headers: { bar: 'foo' } };
+      const request: models.Request = { url: 'foo', headers: { bar: 'foo' } };
       await attachDefaultHeaders(request, {
         config: {
           defaultHeaders: {
@@ -27,7 +27,7 @@ describe('attachDefaultHeaders', () => {
       expect(request.headers?.bar).toEqual('foo');
     });
     it('do not override existing headers', async () => {
-      const request: models.Request = { headers: { foo: 'foo' } };
+      const request: models.Request = { url: 'foo', headers: { foo: 'foo' } };
       await attachDefaultHeaders(request, {
         config: {
           defaultHeaders: {
