868cd9edcf3bc13b9c007cb76c2937309c042437
Andreas Weber <weber.andreas@gmail.com>
2023-09-09 20:54:08 +0200

    fix: update tests to support change in assert

--- src/test/parser/parser.spec.ts
+++ src/test/parser/parser.spec.ts
@@ -56,16 +56,14 @@ describe('parser', () => {
     expect(await httpFile.fileName).toBe('any.http');
     expect(await httpFile.httpRegions.length).toBe(1);
 
-
     for (const httpRegion of httpFile.httpRegions) {
       expect(httpRegion.request).toBeDefined();
       expect(httpRegion.request?.url).toBe('https://httpbin.org/anything');
       expect(httpRegion.symbol.children?.length).toBe(2);
-      expect(httpRegion.symbol.children[0].name).toBe('    GET https://httpbin.org/anything');
-      expect(httpRegion.symbol.children[0].description).toBe('HTTP request-line');
-      expect(httpRegion.symbol.children[1].name).toBe('script');
-      expect(httpRegion.symbol.children[1].description).toBe('script');
-
+      expect(httpRegion.symbol.children?.[0].name).toBe('    GET https://httpbin.org/anything');
+      expect(httpRegion.symbol.children?.[0].description).toBe('HTTP request-line');
+      expect(httpRegion.symbol.children?.[1].name).toBe('assert status');
+      expect(httpRegion.symbol.children?.[1].description).toBe('== 200');
     }
   });
 });
