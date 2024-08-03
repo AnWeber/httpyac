a48d41758ffaf9d02da12b32cbeb6cc6bbda0eb7
Andreas Weber <weber.andreas@gmail.com>
2024-08-03 11:21:27 +0200

    fix: issue with timeout check

--- src/plugins/grpc/callOptionsRequestHook.ts
+++ src/plugins/grpc/callOptionsRequestHook.ts
@@ -5,8 +5,8 @@ import { isGrpcRequest } from './grpcRequest';
 export async function callOptionsRequestHook(request: models.Request, context: models.ProcessorContext): Promise<void> {
   if (isGrpcRequest(request) && request.headers) {
     utils.report(context, 'create call options');
-    if (context.config?.request?.timeout) {
-      const timeout = request.timeout || utils.toNumber(context.config?.request?.timeout) || 0;
+    const timeout = request.timeout || utils.toNumber(context.config?.request?.timeout);
+    if (timeout !== undefined) {
       request.callOptions = Object.assign({}, request.options, {
         deadline: new Date(new Date().getTime() + timeout),
       });
