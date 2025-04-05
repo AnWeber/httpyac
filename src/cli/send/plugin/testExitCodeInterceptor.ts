4e3b601651d044e53caef77e7a80df2bc4d3c61d
Barbara Lócsi <locsi.barbara@gmail.com>
2025-04-05 17:15:22 +0200

    fix: use different exit codes for errored and failed tests

--- src/cli/send/plugin/testExitCodeInterceptor.ts
+++ src/cli/send/plugin/testExitCodeInterceptor.ts
@@ -12,12 +12,30 @@ export const testExitCodeInterceptor = {
 
   afterTrigger: async function bail(hookContext: HookTriggerContext<[models.ProcessorContext], boolean>) {
     const context = hookContext.args[0];
-    const hasFailedOrErroredTestResult = context.httpRegion.testResults?.some?.(
-      obj => obj.status === models.TestResultStatus.FAILED || obj.status === models.TestResultStatus.ERROR
-    );
-    if (hasFailedOrErroredTestResult) {
+
+    if (context.httpRegion.testResults === undefined) {
+      return true;
+    }
+
+    let hasErroredTestResult = false;
+    let hasFailedTestResult = false;
+
+    for (const testResult of context.httpRegion.testResults) {
+      if (testResult.status === models.TestResultStatus.ERROR) {
+        hasErroredTestResult = true;
+        break;
+      }
+      if (testResult.status === models.TestResultStatus.FAILED) {
+        hasFailedTestResult = true;
+      }
+    }
+
+    if (hasErroredTestResult) {
+      process.exitCode = 19;
+    } else if (hasFailedTestResult) {
       process.exitCode = 20;
     }
+
     return true;
   },
 };
